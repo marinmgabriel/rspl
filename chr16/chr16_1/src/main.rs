04118d0e802b6a147aa04f08a436ac5c4e511555
Gabriel Marin <gbrlmarn@gmail.com>
2023-06-16 23:13:32 +0300

    chr16_1

--- chr16/chr16_1/src/main.rs
+++ chr16/chr16_1/src/main.rs
@@ -16,4 +16,15 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
 
+    {
+        let v = vec![1, 2, 3];
+        
+        let handle = thread::spawn(move || {
+            println!("Here's the vector: {:?}", v);
+        });
+
+        handle.join().unwrap();
+    }
+
+
 }
