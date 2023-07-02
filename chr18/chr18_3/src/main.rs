338f6bf258273df42ced6e6827f5ed00ab956c2f
Gabriel Marin <gbrlmarn@proton.me>
2023-07-02 23:15:23 +0300

    chr18_3

---
+++ chr18/chr18_3/src/main.rs
@@ -0,0 +1,92 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Spatial_Point {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    // Destructuring structs
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point {x, y: 0 } => println!("On the x axis at {x}"),
+        Point {x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    // Destructuring enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}",)
+        }
+    }
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
+
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_) = s {
+        println!("Found a string");
+    }
+
+    println!("{:?}", s);
+
+    let origin = Spatial_Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Spatial_Point { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+}
